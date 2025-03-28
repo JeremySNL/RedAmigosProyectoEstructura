398d44769932533901172bd8fb650e6ceaf90e7a
Jeremy Sanchez Cabrera <elninogeremy@gmail.com>
2025-03-27 20:43:03 -0400

    Creando la clase nodo Persona

---
+++ RedAmigosProyectoEstructura/Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedAmigosPersonal
+{
+    internal class Persona
+    {
+        public string _nombre;
+        public string _apellido;
+        public int _edad;
+        public string _numeroTelefonico;
+        public string _email;
+        public Persona(string nombre, string apellido, int edad, string numeroTelefonico, string email)
+        {
+            _nombre = nombre;
+            _apellido = apellido;
+            _edad = edad;
+            _numeroTelefonico = numeroTelefonico;
+            _email = email;
+        }
+    }
+}
